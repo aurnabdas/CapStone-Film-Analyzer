7fb2885177060f3207c869b4ee7341c29b6beec8
mahathirrojan <torbengalifreind@gmail.com>
2024-12-12 05:07:19 -0500

    Icon Integration, Parallax, and Features Rework

--- Frontend/film-analyzer/app/about/page.js
+++ Frontend/film-analyzer/app/about/page.js
@@ -4,36 +4,50 @@ import NavBar from "../../components/NavBar";
 
 const AboutUs = () => {
   return (
-    <div className="bg-[#7A1C23] min-h-screen text-white body-font">
-      <NavBar /> {/* NavBar with matching red background */}
-      
-      <div className="flex flex-col items-center justify-center">
-        <section className="w-full">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="flex flex-col text-center w-full mb-20 bg-[#7A1C23] rounded-lg p-8 shadow-lg">
-              <h1 className="text-2xl font-medium title-font mb-4 tracking-widest" style={{ color: '#D4A03A' }}>
-                WHY WE MADE THIS PROJECT
-              </h1>
-              <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-white">
-                Our Film Analyzer project was built specifically for movie studios seeking a better understanding of audience reactions to their trailers. By combining AI-driven insights with real-time audience feedback, this tool allows studios to gauge interest, predict box office success, and understand emotional engagement at key moments. With features like emotion tracking, AI-generated questions, and custom surveys, Film Analyzer empowers studios to tailor their marketing strategies, determine sequel potential, and make data-backed decisions on future projects.
-              </p>
-            </div>
-
-            <div className="flex flex-col text-center w-full mb-10 bg-[#7A1C23] rounded-lg p-8 shadow-lg">
-              <h2 className="text-2xl font-medium title-font mb-4 tracking-widest" style={{ color: '#D4A03A' }}>
-                WHO CAN USE THIS WEBSITE
-              </h2>
-              <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-white">
-                This platform is designed for both movie studios and everyday users. Movie studios can leverage Film Analyzer to gain deep insights into how audiences react to their trailers, helping them refine marketing strategies, make data-driven decisions, and improve their content.
-              </p>
-              <p className="lg:w-2/3 mx-auto leading-relaxed text-base mt-4 text-white">
-                For regular users, Film Analyzer serves as a hub to explore upcoming movies, watch trailers, and learn more about films through reviews and ratings. Users can dive into details, get a sense of movie trends, and even contribute their own feedback on trailers, making it an engaging experience for movie enthusiasts and researchers alike.
-              </p>
-            </div>
-          </div>
-
-          <Team /> {/* This will render the Team component below the AboutUs content */}
-        </section>
+    <div className="bg-[#450a0a] min-h-screen text-white body-font">
+      <NavBar /> {/* NavBar with the same background */}
+      <div className="container px-5 py-24 mx-auto">
+        {/* Why We Made This Project */}
+        <div className="flex flex-col text-center w-full mb-12 bg-[#7E1328] rounded-lg p-8 shadow-lg border border-gold transition-transform transform hover:scale-105">
+          <h1 className="text-3xl font-bold text-gold mb-6 tracking-wide">
+            WHY WE MADE THIS PROJECT
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-lg text-gray-300">
+            Our Film Analyzer project was built specifically for movie studios
+            seeking a better understanding of audience reactions to their
+            trailers. By combining AI-driven insights with real-time audience
+            feedback, this tool allows studios to gauge interest, predict box
+            office success, and understand emotional engagement at key moments.
+          </p>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-lg mt-4 text-gray-300">
+            With features like emotion tracking, AI-generated questions, and
+            custom surveys, Film Analyzer empowers studios to tailor their
+            marketing strategies, determine sequel potential, and make
+            data-backed decisions on future projects.
+          </p>
+        </div>
+        {/* Who Can Use This Website */}
+        <div className="flex flex-col text-center w-full mb-12 bg-[#7E1328] rounded-lg p-8 shadow-lg border border-gold transition-transform transform hover:scale-105">
+          <h2 className="text-3xl font-bold text-gold mb-6 tracking-wide">
+            WHO CAN USE THIS WEBSITE
+          </h2>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-lg text-gray-300">
+            This platform is designed for both movie studios and everyday users.
+            Movie studios can leverage Film Analyzer to gain deep insights into
+            how audiences react to their trailers, helping them refine marketing
+            strategies, make data-driven decisions, and improve their content.
+          </p>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-lg mt-4 text-gray-300">
+            For regular users, Film Analyzer serves as a hub to explore upcoming
+            movies, watch trailers, and learn more about films through reviews
+            and ratings. Users can dive into details, get a sense of movie
+            trends, and even contribute their own feedback on trailers, making
+            it an engaging experience for movie enthusiasts and researchers
+            alike.
+          </p>
+        </div>
+        {/* Team Section */}
+        <Team /> {/* Same styled Team.js component */}
       </div>
     </div>
   );
--- Frontend/film-analyzer/components/Features.js
+++ Frontend/film-analyzer/components/Features.js
@@ -1,108 +1,87 @@
+"use client";
 import Link from "next/link";
-import ContactForm from '../components/ContactForm'
 
 const Features = () => {
+  const features = [
+    {
+      title: "Real-Time Emotion Tracking",
+      description:
+        "Track viewers' emotions as they watch movie trailers, providing insights into how scenes affect audiences in real-time.",
+      link: "https://face-specs.vercel.app/",
+      image: "/images/Icon One.png", // Replace with your icon image paths
+    },
+    {
+      title: "Predictive Analytics",
+      description:
+        "Use historical data and real-time reactions to predict box office success before a movie hits theaters.",
+      link: "/predictor",
+      image: "/images/group1.png", // Replace with your icon image paths
+    },
+    {
+      title: "Personalized Surveys",
+      description:
+        "Collect tailored feedback from viewers to understand what resonates with them and improve your films.",
+      link: "/survey",
+      image: "/images/group2.png", // Replace with your icon image paths
+    },
+  ];
+
   return (
-    <section className="bg-[url('../public/images/red_curtains.jpeg')] bg-cover bg-center h-screen text-white body-font flex items-center justify-center">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="text-center mb-20">
-          <h1 className="text-[100px] font-bold text-[#D5A036] mb-4">Want Better Movies?</h1>
-          <p className="text-[24px] leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-white">
-            We have the right tools for both users and studios alike where better movies can be produced and shipped out to theaters for them to be viewed.
+    <section className="py-20 bg-[#450a0a] text-white body-font">
+      <div className="container px-5 mx-auto">
+        <div className="text-center mb-12">
+          <h1 className="text-5xl font-bold text-gold mb-4">
+            Want Better Movies?
+          </h1>
+          <p className="text-lg leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-300">
+            We have the right tools for both users and studios alike where
+            better movies can be produced and shipped out to theaters for them
+            to be viewed.
           </p>
           <div className="flex mt-6 justify-center">
-            <div className="w-16 h-1 rounded-full bg-[#D5A036] inline-flex"></div>
+            <div className="w-16 h-1 rounded-full bg-gold inline-flex"></div>
           </div>
         </div>
-        <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6 justify-center">
-          {/* Real-Time Emotion Tracking Feature */}
-          <div className="p-4 md:w-1/3 flex flex-col text-center items-center bg-black bg-opacity-50 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl">
-            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-yellow-100 text-yellow-500 mb-5 flex-shrink-0">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-10 h-10"
-                viewBox="0 0 24 24"
-              >
-                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-              </svg>
-            </div>
-            <div className="flex-grow">
-              <h2 className="text-[#D5A036] text-[36px] font-medium mb-3">Real-Time Emotion Tracking</h2>
-              <p className="text-[18px] leading-relaxed text-white">
-                Track viewers' emotions as they watch movie trailers, providing insights into how scenes affect audiences in real-time.
-              </p>
-              <Link href="https://face-specs.vercel.app/" target="_blank" rel="noopener noreferrer" className="mt-3 text-[#FFD700] inline-flex items-center">
-                Start Tracking Emotions →
-              </Link>
-            </div>
-          </div>
-
-          {/* Predictive Analytics Feature */}
-          <div className="p-4 md:w-1/3 flex flex-col text-center items-center bg-black bg-opacity-50 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl">
-            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-yellow-100 text-yellow-500 mb-5 flex-shrink-0">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-10 h-10"
-                viewBox="0 0 24 24"
-              >
-                <circle cx="6" cy="6" r="3"></circle>
-                <circle cx="6" cy="18" r="3"></circle>
-                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-              </svg>
-            </div>
-            <div className="flex-grow">
-              <h2 className="text-[#D5A036] text-[36px] font-medium mb-3">Predictive Analytics</h2>
-              <p className="text-[18px] leading-relaxed text-white">
-                Use historical data and real-time reactions to predict box office success before a movie hits theaters.
-              </p>
-              <Link href="/predictor" className="mt-3 text-[#FFD700] inline-flex items-center">
-                Predict Movie Success →
-              </Link>
-            </div>
-          </div>
-
-          {/* Personalized Surveys Feature */}
-          <div className="p-4 md:w-1/3 flex flex-col text-center items-center bg-black bg-opacity-50 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl">
-            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-yellow-100 text-yellow-500 mb-5 flex-shrink-0">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-10 h-10"
-                viewBox="0 0 24 24"
-              >
-                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </svg>
-            </div>
-            <div className="flex-grow">
-              <h2 className="text-[#D5A036] text-[36px] font-medium mb-3">Personalized Surveys</h2>
-              <p className="text-[18px] leading-relaxed text-white">
-                Collect tailored feedback from viewers to understand what resonates with them and improve your films.
-              </p>
-              <Link href="/survey" className="mt-3 text-[#FFD700] inline-flex items-center">
-                Get Viewer Feedback →
-              </Link>
-            </div>
-          </div>
+        <div className="flex flex-row flex-wrap justify-center gap-8">
+          {features.map((feature, index) => (
+            <FeatureCard key={index} feature={feature} />
+          ))}
         </div>
-        <button className="flex mx-auto mt-16 text-white bg-[#D5A036] border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">
-          Learn More About Our Tools
-        </button>
-        
+        <div className="flex justify-center mt-12">
+          <button className="text-white bg-gold py-2 px-8 rounded-lg hover:bg-[#9b2222] transition duration-300">
+            Learn More About Our Tools
+          </button>
+        </div>
       </div>
     </section>
   );
 };
+
+function FeatureCard({ feature }) {
+  return (
+    <div className="flex flex-col text-center items-center bg-[#7E1328] shadow-lg rounded-lg overflow-hidden border border-gold transition-transform transform hover:scale-105 hover:shadow-[0_0_20px_10px_rgba(255,223,0,0.9)] px-6 py-8">
+      <img
+        src={feature.image}
+        alt={feature.title}
+        className="w-24 h-24 object-contain invert" // Larger size and white color
+      />
+      <div className="mt-4">
+        <h2 className="text-2xl font-medium text-gold mb-3">{feature.title}</h2>
+        <p
+          className="text-gray-300 text-sm mx-auto"
+          style={{ maxWidth: "250px" }}
+        >
+          {feature.description}
+        </p>
+        <Link
+          href={feature.link}
+          className="mt-3 text-gold inline-flex items-center font-semibold hover:underline"
+        >
+          Learn More →
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 export default Features;
